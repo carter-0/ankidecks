9eaac93254ff29c2d35d68b8389bfa4232d68e4b
carter-0 <carterannandale@gmail.com>
2023-05-31 21:03:18 +0100

    fix: try new things

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     debug: true,
     publicRoutes: ["/"],
-    domain: "https://ankidecks.app" as undefined
 });
 
 export const config = {
