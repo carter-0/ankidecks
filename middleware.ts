988ababfbebcc79342279b32dcc34fd2a3e37367
carter-0 <carterannandale@gmail.com>
2023-05-31 20:36:20 +0100

    fix: try new things

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+    debug: true,
+    publicRoutes: ["/"],
+    proxyUrl: "https://ankidecks.app"
+});
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
