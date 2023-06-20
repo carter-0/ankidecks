93b4d13bfc426e63058ac8c48e3ec5f7c53246a1
carter-0 <carterannandale@gmail.com>
2023-06-20 13:58:03 +0100

    fix: changes to middleware

--- middleware.ts
+++ middleware.ts
@@ -10,17 +10,12 @@
 //
 // export default authMiddleware();
 //
-// export const config = {
-//     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
 
-import {authMiddleware, redirectToSignIn} from '@clerk/nextjs';
 
-export default authMiddleware({
-    afterAuth(auth, req, evt) {
-        // handle users who aren't authenticated
-        if (!auth.userId && !auth.isPublicRoute) {
-            return redirectToSignIn({ returnBackUrl: req.url });
-        }
-    }
-});
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware();
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
