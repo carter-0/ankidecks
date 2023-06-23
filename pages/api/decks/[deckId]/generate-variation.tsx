31a03a4e9b93b9729545a9a580533f82890ceb82
carter-0 <carterannandale@gmail.com>
2023-06-23 18:13:37 +0100

    feat: add /api/deck/<id>/generate-variation

---
+++ pages/api/decks/[deckId]/generate-variation.tsx
@@ -0,0 +1,56 @@
+import {getAuth} from "@clerk/nextjs/server";
+import {prisma} from "@/lib/db";
+import {sendRabbitMQMessage} from "@/lib/rabbitmq";
+
+export default async function handler(req: any, res: any) {
+    const {userId} = getAuth(req);
+    const {deckId} = req.query;
+    const {onlyGenerateFirstField} = JSON.parse(req.body);
+
+    if (!deckId) {
+        return res.status(400).json({success: false, message: "Missing deckId"});
+    }
+
+    if (!userId) {
+        return res.status(401).json({success: false, message: "Unauthorized"});
+    }
+
+    let firstField = false;
+
+    if (onlyGenerateFirstField === true) {
+        firstField = true;
+    }
+
+    const deck = await prisma.deck.findUnique({
+        where: {
+            id: deckId
+        }
+    }) as Deck;
+
+    if (!deck || deck.user !== userId) {
+        return res.status(401).json({success: false, message: "Unauthorized"});
+    }
+
+    const task = await prisma.task.create({
+        data: {
+            userId: userId,
+            type: 'VARIATION',
+            title: `Generating variation of ${deck.name}`,
+            deck: {
+                connect: {
+                    id: deckId
+                }
+            },
+            status: 'PENDING',
+            startedAt: new Date()
+        }
+    }) as Task;
+
+    await sendRabbitMQMessage({
+        type: 'VARIATION',
+        deckId: `${deckId}`,
+        firstField: firstField,
+    })
+
+    return res.status(200).json({success: true});
+}
